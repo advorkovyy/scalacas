f0d40f37e76f4e7b6fc281e47dd63823c9369c88
Alexander Dvorkovyy <advorkovyy@gmail.com>
2011-04-13 14:11:54 +0200

    Moved to ProtobufMapper

---
+++ src/main/scala/org/scalastuff/scalacas/beans/ProtobufMapper.scala
@@ -0,0 +1,51 @@
+package org.scalastuff.scalacas.beans
+
+import org.scalacas.{Mapper, HasIdSupport, HasId}
+import org.scalacas.serialization.Serializers._
+import org.apache.cassandra.thrift.Column
+import org.scale7.cassandra.pelops.{Bytes, Mutator}
+import java.util.Arrays
+import com.dyuproject.protostuff.{Schema, LinkedBuffer, ProtobufIOUtil}
+import org.scalastuff.proto.MirrorSchema
+
+/**
+ * Maps JavaBeans of given class to JSON string and saves it in '-json' column. <p>
+ *
+ * Doesn't support Scala types like Option, BigDecimal etc. Doesn't
+ * support Scala properties, use {@code @BeanProperty}<p>
+ *
+ * For further details see http://jackson.codehaus.org/
+ *
+ * @author Alexander Dvorkovyy
+ * @see <a href="http://jackson.codehaus.org/">http://jackson.codehaus.org/</a>
+ *
+ */
+class ProtobufMapper[A <: HasId](prefix:String)(implicit mf:Manifest[A]) extends AbstractProtobufMapper[A](prefix) with HasIdSupport[A]
+
+abstract class AbstractProtobufMapper[A <: AnyRef](prefix:String)(implicit mf:Manifest[A]) extends Mapper[A](prefix) {
+	import ProtobufMapper._
+
+  val schema: Schema[A] = MirrorSchema.schemaOf[A]
+  // TODO: must be thread local!!!!
+  val linkedBuffer = LinkedBuffer.allocate(512)
+
+	def objectToColumns(mutator:Mutator, obj:A):Seq[Column] = {
+		Seq(mutator.newColumn(PROTOBUF_BYTES, Bytes.fromByteArray(ProtobufIOUtil.toByteArray(obj, schema, linkedBuffer))))
+	}
+
+	def columnsToObject(subColumns:Seq[Column]):A = {
+		subColumns find ( col => Arrays.equals(PROTOBUF_BYTES.getBytes.array, col.getName) ) match {
+			case Some(col) =>
+				//println(fromBytes[String](col.getValue))
+        val obj = schema.newMessage()
+        ProtobufIOUtil.mergeFrom(col.getValue, obj, schema)
+				obj
+			case None => throw new RuntimeException("Cannot deserialize object: column with name '%s' not found".format(PROTOBUF))
+		}
+	}
+}
+
+object ProtobufMapper {
+	val PROTOBUF = "-protobuf"
+	val PROTOBUF_BYTES = toBytes(PROTOBUF)
+}
--- src/test/scala/org/scalastuff/scalacas/beans/ReflectionPerformanceTest.scala
+++ src/test/scala/org/scalastuff/scalacas/beans/ReflectionPerformanceTest.scala
@@ -1,9 +1,7 @@
-package org.scalacas.reflection
+package org.scalastuff.scalacas.beans
 
 import scala.reflect.BeanProperty
-import ScalaReflection._
 import org.scalacas.{ Mapper, HasId, HasIdSupport }
-import org.scalacas.json.JsonMapper
 import org.scale7.cassandra.pelops.{ Mutator, Bytes }
 import org.apache.cassandra.thrift.Column
 import org.specs.SpecificationWithJUnit
@@ -20,33 +18,24 @@ class ReflectionTest extends HasId {
 }
 
 class ReflectionTestBean extends HasId {
-  @BeanProperty var i: Int = 1
-  @BeanProperty var i2: Integer = 2
-  @BeanProperty var s: String = "whatever"
-  @BeanProperty var l: Long = 3
-  @BeanProperty var bd: java.math.BigDecimal = new java.math.BigDecimal("2.33")
+  @BeanProperty
+  var i: Int = 1
+  @BeanProperty
+  var i2: Integer = 2
+  @BeanProperty
+  var s: String = "whatever"
+  @BeanProperty
+  var l: Long = 3
+  @BeanProperty
+  var bd: java.math.BigDecimal = new java.math.BigDecimal("2.33")
   def id = i.toString
 }
 
 @RunWith(classOf[JUnitSuiteRunner])
 class ReflectionPerformanceTest extends SpecificationWithJUnit {
 
-  "test reflection performance" in {
-    val rt = new ReflectionTest
-    val properties = rt.getClass.properties filter (!_.isReadOnly)
-    val start = System.nanoTime
-    for (i <- 1 to 1000000; p <- properties) {
-      val value = p.get[AnyRef](rt)
-      //			p.set(rt, value)
-      //			val buf = Bytes.fromUTF8("")
-      //			buf.toUTF8
-    }
-
-    println("get via reflection 1M times: %d ms".format((System.nanoTime - start) / 1000000))
-  }
-  
   "test mapping performance" in {
-    val rt = new ReflectionTest
+    
     val mapper = new Mapper[ReflectionTest]("") with HasIdSupport[ReflectionTest] {
       def objectToColumns(mutator: Mutator, obj: ReflectionTest): Seq[Column] = throw new RuntimeException
       def columnsToObject(subColumns: Seq[Column]): ReflectionTest = {
@@ -62,49 +51,40 @@ class ReflectionPerformanceTest extends SpecificationWithJUnit {
         result
       }
     }
-    val reflectionMapper = new ReflectionMapper[ReflectionTest]("", new ReflectionTest)
-    val polyMapper = new PolymorphicReflectionMapper[ReflectionTest]("")
+    val protoMapper = new ProtobufMapper[ReflectionTest]("")
 
+    val rt = new ReflectionTest
     rt.i = 10
     rt.i2 = 20
     rt.s = "xxx aaa7"
     rt.l = 30
     rt.bd = 45.78
 
-    val mutator = new Mutator(null, 0, false)
-    val scl = polyMapper.objectToColumns(mutator, rt)
-    scl.size must be(6)
+//    val cmutator = new Mutator(null, 0, false)
+//    val cscl = mapper.objectToColumns(cmutator, rt)
+//    cscl.size must be(5)
 
-    val obj = reflectionMapper.columnsToObject(scl)
+    val pmutator = new Mutator(null, 0, false)
+    val pscl = protoMapper.objectToColumns(pmutator, rt)
+
+    val obj = protoMapper.columnsToObject(pscl)
     obj.i must be equalTo (rt.i)
     obj.i2 must be equalTo (rt.i2)
     obj.s must be equalTo (rt.s)
     obj.l must be equalTo (rt.l)
     obj.bd must be equalTo (rt.bd)
-    
-    val rtb = new ReflectionTestBean
-    val jsonMapper = new JsonMapper("", classOf[ReflectionTestBean])
-	   
-	  val jmutator = new Mutator(null, 0, false)
-      val sclj = jsonMapper.objectToColumns(jmutator, rtb)
 
-    testMapperPerformance("custom mapper", mapper, scl)
-    testMapperPerformance("reflection mapper", reflectionMapper, scl)
-    testMapperPerformance("polymorphic mapper", polyMapper, scl)
-    testMapperPerformance("JSON mapper", jsonMapper, sclj)
-
-    testMapperPerformance("reflection mapper", reflectionMapper, scl)
-    testMapperPerformance("polymorphic mapper", polyMapper, scl)
-    testMapperPerformance("custom mapper", mapper, scl)
-    testMapperPerformance("JSON mapper", jsonMapper, sclj)
-
+    for (i <- 1 to 5) {
+//	    testMapperPerformance("custom mapper", mapper, cscl)
+	    testMapperPerformance("proto mapper", protoMapper, pscl)
+    }
   }
 
   private def testMapperPerformance[A <: AnyRef](mapperName: String, mapper: Mapper[A], scl: Seq[Column]) {
     System.gc()
     val start = System.nanoTime
     for (i <- 1 to 1000000) {
-//    val scl = mapper.toSubColumnsList(mutator, rt)
+      //    val scl = mapper.toSubColumnsList(mutator, rt)
       val obj = mapper.columnsToObject(scl)
     }
 
