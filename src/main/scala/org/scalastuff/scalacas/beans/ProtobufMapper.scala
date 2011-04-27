5fb54492c26eee260977229d22f441d1539b42a6
Alexander Dvorkovyy <advorkovyy@gmail.com>
2011-04-27 10:22:27 +0200

    ProtobufMapper updated to the latest ScalaBean version

--- src/main/scala/org/scalastuff/scalacas/beans/ProtobufMapper.scala
+++ src/main/scala/org/scalastuff/scalacas/beans/ProtobufMapper.scala
@@ -5,11 +5,8 @@ import org.scalastuff.scalacas.serialization.Serializers._
 import org.apache.cassandra.thrift.Column
 import org.scale7.cassandra.pelops.{ Bytes, Mutator }
 import java.util.Arrays
-import com.dyuproject.protostuff.{ Schema, LinkedBuffer, ProtobufIOUtil }
-import org.scalastuff.scalabeans.Preamble._
-import org.scalastuff.proto.value.BeanValueHandler
-import com.dyuproject.protostuff.ProtobufOutput
-import com.dyuproject.protostuff.ByteArrayInput
+import org.scalastuff.proto.Preamble._
+import org.scalastuff.proto._
 
 /**
  * Maps JavaBeans of given class to protobuf byte array and saves it in '-protobuf' column. <p>
@@ -21,23 +18,20 @@ class ProtobufMapper[A <: HasId](prefix: String)(implicit mf: Manifest[A]) exten
 abstract class AbstractProtobufMapper[A <: AnyRef](prefix: String)(implicit mf: Manifest[A]) extends Mapper[A](prefix) {
   import ProtobufMapper._
 
-  val beanValueHandler = BeanValueHandler(scalaTypeOf[A])
+  val reader = readerOf[A]
+  val writer = writerOf[A]
+  val format = ProtobufFormat
 
   def objectToColumns(mutator: Mutator, obj: A): Seq[Column] = {
-    linkedBuffer.clear()
-    val output = new ProtobufOutput(linkedBuffer)
-    beanValueHandler.schemaWriteTo(output, obj)
-    Seq(mutator.newColumn(PROTOBUF_BYTES, Bytes.fromByteArray(output.toByteArray)))
+    Seq(mutator.newColumn(PROTOBUF_BYTES, Bytes.fromByteArray(writer.toByteArray(obj, format))))
   }
 
   def columnsToObject(superColumnName: Array[Byte], subColumns: Seq[Column]): A = {
     subColumns find (col => Arrays.equals(PROTOBUF_BYTES.getBytes.array, col.getName)) match {
       case Some(col) =>
-        //println(fromBytes[String](col.getValue))
-      	// TODO: exception if cannot create instance without constructor parameters. cannot we use immutable schema here?
-        val input = new ByteArrayInput(col.getValue, false)
-        beanValueHandler.schemaReadFrom(input).asInstanceOf[A]
-      case None => throw new RuntimeException("Cannot deserialize object: column with name '%s' not found".format(PROTOBUF))
+        reader.readFrom(col.getValue, ProtobufFormat)
+      case None => 
+        throw new RuntimeException("Cannot deserialize object: column with name '%s' not found".format(PROTOBUF))
     }
   }
 }
@@ -45,10 +39,4 @@ abstract class AbstractProtobufMapper[A <: AnyRef](prefix: String)(implicit mf: 
 object ProtobufMapper {
   val PROTOBUF = "-protobuf"
   val PROTOBUF_BYTES = toBytes(PROTOBUF)
-  
-  val linkedBufferPool = new ThreadLocal[LinkedBuffer] {
-    override protected def initialValue() = LinkedBuffer.allocate(512)
-  }
-  
-  def linkedBuffer = linkedBufferPool.get
 }
